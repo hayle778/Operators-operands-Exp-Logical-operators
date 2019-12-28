6695880d7c7d8b8f447bebffa6e3114c96c8b9a5
yourUsername <yourEmail@domain.com>
2019-12-27 22:51:05 -0500

    OOEL

---
+++ src/main/java/com/example/demo/hello.java
@@ -0,0 +1,159 @@
+/*
+Train my self by doing different class
+
+*/
+package com.example.demo;
+
+public class hello {
+public static void main(String[] args) {
+    System.out.println( "hello team!" );
+    int myFirstnumber = 10 + 5 + (2 * 40);
+    int mySecondnumber = 20;
+    int myThirdnumber = 30 + 20 + 50;
+    int myTotal = myFirstnumber + mySecondnumber + myThirdnumber;
+    System.out.println( myTotal );
+    int myLastone = 1000 - myTotal;
+    System.out.println( myLastone );
+    int myLastlastone = myFirstnumber + mySecondnumber * 23;
+    System.out.println( myLastlastone + myFirstnumber );
+    int myValue = 10000;
+    int myMinIntvalue = Integer.MIN_VALUE;
+    int myMaxIntvalue = Integer.MAX_VALUE;
+    System.out.println( "Integer Minimum Value = " + myMinIntvalue );
+    System.out.println( "Integer Maximum Value = " + myMaxIntvalue );
+    System.out.println( "Busted MAX value = " + (myMaxIntvalue + 1) );
+    System.out.println( "busted MIN value = " + (myMinIntvalue - 1) );
+// learn about the char
+    char myChar = 'D';
+    char myUnicodecHAR = '\u0044';
+    boolean myTrueBooleanValue = true;
+    boolean myFalseBooleanValue = false;
+    //byte
+    // short
+    // int
+    // long
+    //float
+    //double
+    //char
+    //boolean
+    boolean isCustromerOverTwentyOne = true;
+    String myString = "This is a Namoi`s book";
+    System.out.println( "myString is probably equal to " + myString );
+    myString = myString + ", and this ismore.";
+    System.out.println( "myString is equal to " + myString );
+    myString = myString + "\u00A9 2019";
+    System.out.println( "myString is equal to " + myString );
+    myString = myString + "BUY THE MASTER BOOK";
+    System.out.println( "my String is equal to " + myString );
+    myString = myString + "Buy siphara`s Book";
+    System.out.println( "my String final equal to" + myString );
+    // int numberString = "250.55";
+    int lastString = 10;
+    int myInt = 50;
+    lastString = lastString + myInt;
+    System.out.println( "LastString is equal to" + lastString );
+    lastString = lastString + 120;
+    System.out.println( "LastString is equal to" + lastString );
+
+    int myVar = 11 + 45 * 25;
+    System.out.println( myVar );
+    //double mySalary = hoursWorked * hourlyRate;
+//result = result/5;
+    int result2 = 3;
+    int result = 1 + 2;
+    System.out.println( "1+2 = " + result );
+    int previousResult = result;
+    System.out.println( "previousResult = " + previousResult );
+    result = result - 1;
+    System.out.println( "3-1 = " + result );
+    result = result * 2;
+    System.out.println( ("print" + result) );
+    result = result - 44;
+    System.out.println( "print" + result );
+    result = result / 5;
+    System.out.println( "20 /2=" + result );
+    result = result * 4;
+    System.out.println( "res" + result );
+
+    boolean isagooddribbler = false;    // the logic that we feed
+    if (isagooddribbler == false) {
+        System.out.println( "It is not Edenhazard!" );
+        System.out.println( "good job!" );
+
+    }
+//else
+    //  System.out.println( "it is E.Hazard" );
+
+
+//        int topScore = 100;
+//        if (topScore != 100) {
+//            System.out.println( "you got the low score!" );
+//            System.out.println( "Good work n go to the next to write, compile, execute the data or statment that we have" + topScore );
+//        }
+//        else {
+//            System.out.print( "do it again!" );
+//        }
+
+    int topScore = 80;
+if (topScore > 80 ) {
+    System.out.print( "you have got high xxx" );
+
+}
+
+int secondTopScore = 60;
+if ((topScore == secondTopScore) && (topScore < 100)) {
+System.out.print( "not equal to the second Top score and less than 100" );
+}
+
+if((topScore<=80)|| (secondTopScore<70)){
+    System.out.print( "Either or both of the conditions are true" );
+}
+
+int newValue = 50;
+if(newValue == 50){
+    System.out.print( "This is an error" );
+}
+boolean isCar = false;
+if(isCar==false) {
+    System.out.print( "This is suppose to happen" );
+}
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
